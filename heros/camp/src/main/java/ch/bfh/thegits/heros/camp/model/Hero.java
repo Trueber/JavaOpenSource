eee9f86ebca2733af7cbb5f513d38ec43ad74f9d
Fabian Wittwer <fwittwer@hotmail.com>
2019-11-07 17:13:46 +0100

    Implemented Hero persistence on HIbernate

--- heros/camp/src/main/java/ch/bfh/thegits/heros/camp/model/Hero.java
+++ heros/camp/src/main/java/ch/bfh/thegits/heros/camp/model/Hero.java
@@ -1,12 +1,23 @@
 package ch.bfh.thegits.heros.camp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Hero
 {
+    @Id
+    @GeneratedValue( generator = "system-uuid" )
+    @GenericGenerator( name = "system-uuid", strategy = "uuid" )
+    private String id;
+
     private String name;
     private int atk;
     private int def;
