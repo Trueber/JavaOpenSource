3d9fe78add103634a7db9b8f2d7e7fbddde36318
Fabian Wittwer <fwittwer@hotmail.com>
2019-11-15 18:55:13 +0100

    Adapted Campmodule to SpringBoot

---
+++ heros/camp/src/test/java/ch/bfh/thegits/heros/camp/CampApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.bfh.thegits.heros.camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
