111708ccbf76eb34537a99e7f0febc5a3cc70350
Fabian Wittwer <fwittwer@hotmail.com>
2019-11-28 16:43:44 +0100

    Implemented Spring MVC exercise using spring-data-rest

--- heros/camp/src/main/java/ch/bfh/thegits/heros/camp/model/Party.java
+++ heros/camp/src/main/java/ch/bfh/thegits/heros/camp/model/Party.java
@@ -1,11 +1,12 @@
 package ch.bfh.thegits.heros.camp.model;
 
 import lombok.Data;
+import org.springframework.hateoas.ResourceSupport;
 
 import java.util.List;
 
 @Data
-public class Party
+public class Party extends ResourceSupport
 {
     private String name;
     private List<Hero> members;
