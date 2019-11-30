53f8ff6d2639a4fd5559d9b18c35d3ab2da35ab7
Fabian Wittwer <fwittwer@hotmail.com>
2019-11-30 18:45:55 +0100

    Added promoter module stub

---
+++ heros/promoter/src/main/java/ch/bfh/thegis/heros/promoter/PromoterApplication.java
@@ -0,0 +1,15 @@
+package ch.bfh.thegis.heros.promoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoterApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( PromoterApplication.class, args );
+    }
+
+}
---
+++ heros/promoter/src/test/java/ch/bfh/thegis/heros/promoter/PromoterApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.bfh.thegis.heros.promoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoterApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
