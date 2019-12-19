62547fafaaec3601fa89edcbdbcbd78718cce5ef
Fabian Wittwer <fwittwer@hotmail.com>
2019-12-19 17:08:04 +0100

    added eureka client to arena

--- heros/arena/src/main/java/ch/bfh/thegits/heros/arena/ArenaApplication.java
+++ heros/arena/src/main/java/ch/bfh/thegits/heros/arena/ArenaApplication.java
@@ -2,14 +2,14 @@ package ch.bfh.thegits.heros.arena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArenaApplication
 {
-
     public static void main( String[] args )
     {
         SpringApplication.run( ArenaApplication.class, args );
     }
-
 }
